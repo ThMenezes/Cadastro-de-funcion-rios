3e540befaa0f7226a8c0250ab48b4ba285418bba
ThMenezes <thita.menezes24@gmail.com>
2022-12-28 11:47:15 -0300

    criando função de dados de funcionários

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,22 @@ document.getElementById('cadastrarFuncionario')
 
 document.getElementById("modalClose")
 .addEventListener("click", closeModal)
+
+const informacoesFuncionarios = {
+    nome: "Ryan",
+    email: "ryan@gmail.com",
+    celular: "11967583922",
+    cargo: "Chefe",
+    cidade: "São Paulo"
+}
+
+// envio e retorno dos dados ao localStorage //
+const getLocalStorage = () => JSON.parse(localStorage.getItem("dados_funcionarios")) ?? []
+const setLocalStorage = (dadosFuncionarios) => localStorage.setItem("dados_funcionarios", JSON.stringify(dadosFuncionarios))
+
+// crud - Creat- (Criar) //
+const criarFuncionario = (funcionario) => {
+    const dadosFuncionarios = getLocalStorage() 
+    dadosFuncionarios.push (funcionario)
+    setLocalStorage(dadosFuncionarios)   
+}
