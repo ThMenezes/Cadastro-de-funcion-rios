a9f58b77d09b34e75866cb67cbf982aeebde1553
ThMenezes <thita.menezes24@gmail.com>
2022-12-29 09:10:47 -0300

    tarefa: imprimindo informações na tela

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,17 @@
 const openModal = () => document.getElementById('modal')
 .classList.add('active')
 
-const closeModal = () => document.getElementById('modal')
+const closeModal = () => {
+    apagarCampos()
+    document.getElementById('modal')
 .classList.remove('active')
+}
 
 document.getElementById('cadastrarFuncionario')
 .addEventListener("click", openModal)
 
 document.getElementById("modalClose")
 .addEventListener("click", closeModal)
-
-const informacoesFuncionarios = {
-    nome: "Luan",
-    email: "luan@gmail.com",
-    celular: "11967583922",
-    cargo: "Administrativo",
-    cidade: "São Paulo"
-}
 
 // envio e retorno dos dados ao localStorage //
 const getLocalStorage = () => JSON.parse(localStorage.getItem("dados_funcionarios")) ?? []
@@ -43,3 +38,60 @@ const deleteFuncionario = (index) => {
     dadosFuncionarios.splice(index,1)
     setLocalStorage(dadosFuncionarios)
 }
+
+// validando e adicionando evento ao salvar //
+const campoValido = () => {
+    return document.getElementById("formulario").reportValidity()
+}
+
+const apagarCampos = () => {
+    const campos = document.querySelectorAll('.modal-field')
+    campos.forEach(campo => campo.value = "")
+}
+
+const salvarFuncionario = () => {
+    if (campoValido()) {
+        const funcionario = {
+        nome: document.getElementById('nome').value,
+        email:document.getElementById('email').value,
+        celular:document.getElementById('celular').value,
+        cargo:document.getElementById('cargo').value,
+        cidade:document.getElementById('cidade').value
+    }
+    criarFuncionario(funcionario)
+    atualizarTabela()
+     closeModal()
+    }
+}
+
+document.getElementById('salvar')
+.addEventListener("click", salvarFuncionario)
+
+// adicionando informações na tela //
+const criarLinha = (funcionario) => {
+    const novaLinha = document.createElement('tr')
+    novaLinha.innerHTML = `
+    <td>${funcionario.nome}</td>
+    <td>${funcionario.email}</td>
+    <td>${funcionario.celular}</td>
+    <td>${funcionario.cargo}</td>
+    <td>${funcionario.cidade}</td>
+    <td>
+        <button type="button" class="botao editar">Editar</button>
+        <button type="button" class="botao excluir">Excluir</button>
+    </td>`
+    document.querySelector('#tabelaFuncionario>tbody').appendChild(novaLinha)
+}
+
+const apagarTabela = () => {
+    const linhas = document.querySelectorAll('#tabelaFuncionario>tbody tr')
+    linhas.forEach(linha => linha.parentNode.removeChild(linha))
+}
+
+const atualizarTabela = () => {
+    const dadosFuncionarios = readFuncionario()
+    apagarTabela()
+    dadosFuncionarios.forEach(criarLinha)
+}
+
+atualizarTabela()
